9060053363132361e0db6ef00156e42d8d4faa6a
Prashilkumar-Rupapara <prashilkumar.rupapara@gmail.com>
2025-03-14 17:09:41 +0100

    Added function apply_mask_on_2Darray in Masks.py

--- Masks.py
+++ Masks.py
@@ -43,15 +43,13 @@ masks = {
 	],
 }
 
-def apply_mask_on(image_path: str, mask: list[list[float]]):
-	# Load as greyscale image	
-	image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  
-
-	# get the dimensions of image
-	resized_dim = image.shape
-	print(f"Resized dimensions: {resized_dim}")
-	resized_dim = image.shape[:2]
-	print(f"Resized dimensions: {resized_dim}")
+def apply_mask_on_2Darray(array: np.ndarray, mask: list[list[float]]):
+	# raise error if input array is not 2D
+	if array.ndim != 2:
+		raise ValueError("The input array must be 2D.")
+	
+	# get the dimensions of the array
+	resized_dim = array.shape
 
 	# extract data from mask
 	(data_x_min, data_x_max, data_y_min, data_y_max), *rectangles = mask
@@ -64,6 +62,7 @@ def apply_mask_on(image_path: str, mask: list[list[float]]):
 	# Loop through the original rectangles and scale them
 	for (x1, y1, x2, y2) in rectangles:
 		# print(resized_image.shape, (x1, x2, y1, y2))
+
 		# Move the origin of the rectangle to the top-left
 		x1 -= data_x_min
 		x2 -= data_x_min
@@ -80,10 +79,12 @@ def apply_mask_on(image_path: str, mask: list[list[float]]):
 		scaled_x1, scaled_x2 = sorted([scaled_x1, scaled_x2])
 		scaled_y1, scaled_y2 = sorted([scaled_y1, scaled_y2])
 
-		# Overwrite the pixels in the rectangle region with black
-		resized_image[int(scaled_y1):int(scaled_y2), int(scaled_x1):int(scaled_x2)] = 0
+		# Overwrite the values in the rectangle region with 0 - black
+		array[int(scaled_y1):int(scaled_y2), int(scaled_x1):int(scaled_x2)] = 0
 
 		# print(resized_image.shape, (scaled_x1, scaled_y1, scaled_x2, scaled_y2))
+		
+	return array
 
 
 
@@ -98,4 +99,10 @@ def apply_mask_on(image_path: str, mask: list[list[float]]):
 
 
 if __name__ == "__main__":
-	apply_mask_on()
+	from interpolated_greyscale_image_generator import load_npy_file, process_and_group_data, interpolate_nlcreq, save_normalized_greyscale_image
+
+	arr = load_npy_file(r'.\data\SAC105\FCGB1_sac105.npy')
+	grouped_data = process_and_group_data(arr)
+	NLCREQ_interp = interpolate_nlcreq(grouped_data, grid_size=100)
+	masked_NLCREQ_interp = apply_mask_on_2Darray(NLCREQ_interp, masks['FCGB1'])
+	save_normalized_greyscale_image(masked_NLCREQ_interp, r'.\images\masked.png')
